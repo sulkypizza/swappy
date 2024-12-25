f7b3b19028900391c810ec1ce79bd44f96b7d6d6
b1hutch <b1hutch@live.com>
2024-12-25 16:45:22 -0600

    added setting panel (pt. 1)

---
+++ save_switcher/Elements/SimpleButton.cs
@@ -0,0 +1,48 @@
+﻿using SharpDX;
+using SharpDX.Direct2D1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace save_switcher.Elements
+{
+    public struct SimpleButtonProperties
+    {
+        public Vector2 Position;
+        public Size2 Size;
+        public string Text;
+        public float BorderThickness;
+        public Color ButtonColor;
+        public Color ButtonHoverColor;
+        public Color ButtonPressedColor;
+        public Color BorderColor;
+        public Color BorderHoverColor;
+        public Color BorderPressedColor;
+    }
+
+    internal class SimpleButton : Button
+    {
+        public SimpleButton(DeviceContext deviceContext, SimpleButtonProperties properties) 
+            : base (deviceContext, properties.Position, properties.Size)
+        {
+
+        }
+
+        public override void Update()
+        {
+            
+        }
+
+        public override void Deselect()
+        {
+            
+        }
+
+        public override void Draw()
+        {
+
+        }
+    }
+}
---
+++ save_switcher/Panels/Settings.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using SharpDX.Direct2D1;
+
+namespace save_switcher.Panels
+{
+    internal class Settings : Panel
+    {
+        private DeviceContext deviceContext;
+        public Settings(DeviceContext deviceContext)
+        {
+            this.deviceContext = deviceContext;
+
+            createSizeDependantResources();
+        }
+
+        private void createSizeDependantResources()
+        {
+
+        }
+
+        public void Resize(DeviceContext deviceContext)
+        {
+            this.deviceContext = deviceContext;
+            createSizeDependantResources();
+        }
+
+        public void Update()
+        {
+
+        }
+
+        public void Draw(DeviceContext deviceContext)
+        {
+
+        }
+    }
+}
