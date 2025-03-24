aa6810e432721b02e69232a655a61db1f13d0adc
sulkypizza <b1hutch@live.com>
2025-03-23 23:08:20 -0500

    switch button class to use new input system

--- save_switcher/Elements/Button.cs
+++ save_switcher/Elements/Button.cs
@@ -1,5 +1,7 @@
 ﻿using SharpDX;
 using SharpDX.Direct2D1;
+using System;
+using System.Diagnostics;
 
 namespace save_switcher.Elements
 {
@@ -7,8 +9,10 @@ namespace save_switcher.Elements
     public delegate void PressedEvent();
     public delegate void HeldEvent();
 
-    internal abstract class Button : InputNavigable
+    internal abstract class Button : InputNavigable, IDisposable
     {
+        static protected bool inputHandled;
+
         public Size2 Size;
         public Vector2 Position;
 
@@ -16,13 +20,84 @@ namespace save_switcher.Elements
         public PressedEvent OnPressed;
         public HeldEvent OnHeld;
 
+        private bool cursorWithinBounds = false;
+
         protected DeviceContext deviceContext;
+
+        static Button()
+        {
+            InputManager.OnUpInput += (t) => { inputHandled = false; };
+            InputManager.OnDownInput += (t) => { inputHandled = false; };
+            InputManager.OnLeftInput += (t) => { inputHandled = false; };
+            InputManager.OnRightInput += (t) => { inputHandled = false; };
+            InputManager.OnEnterInput += (t) => { inputHandled = false; };
+        }
 
         public Button(DeviceContext deviceContext, Vector2 position, Size2 size)
         {
             this.deviceContext = deviceContext;
             this.Position = position;
             this.Size = size;
+
+            InputManager.OnMousePosChanged += inputMousePosChanged;
+            InputManager.OnLeftMouseInput += inputMouseLeft;
+
+            InputManager.OnLeftInput += (t) => { inputDirection(t, NavigateDirection.Left); };
+            InputManager.OnRightInput += (t) => { inputDirection(t, NavigateDirection.Right); };
+            InputManager.OnUpInput += (t) => { inputDirection(t, NavigateDirection.Up); };
+            InputManager.OnDownInput += (t) => { inputDirection(t, NavigateDirection.Down); };
+
+            InputManager.OnEnterInput += (t) =>
+            {
+                if (t == InputManager.ButtonTravel.Down)
+                    OnPressed?.Invoke();
+            };
+        }
+
+        private void inputDirection(InputManager.ButtonTravel travel, NavigateDirection direction)
+        {
+            if (CurrentSelectedObject == this && travel == InputManager.ButtonTravel.Down 
+                && inputHandled == false)
+            {
+                SelectNeighbor(direction);
+                inputHandled = true;
+            }
+        }
+
+        private void inputMouseLeft(InputManager.ButtonTravel travel)
+        {
+            if (CurrentSelectedObject == this)
+            {
+                if (travel == InputManager.ButtonTravel.Down && cursorWithinBounds)
+                {
+                    OnPressed.Invoke();
+                }
+                else if (travel == InputManager.ButtonTravel.Up)
+                {
+                    OnHover.Invoke();
+                }
+            }
+        }
+
+        private void inputMousePosChanged(System.Drawing.Point p)
+        {
+            Vector2 mousePos = new Vector2(p.X, p.Y);
+
+            if (InputManager.CurrentInputType == InputManager.InputType.Mouse && mousePos.X >= Position.X &&
+                mousePos.X <= Position.X + Size.Width && mousePos.Y >= Position.Y &&
+                mousePos.Y <= Position.Y + Size.Height)
+            {
+                if (CurrentSelectedObject != this)
+                {
+                    Select();
+                }
+
+                cursorWithinBounds = true;
+            }
+            else
+            {
+                cursorWithinBounds = false;
+            }
         }
 
         public override void Select()
@@ -35,6 +110,13 @@ namespace save_switcher.Elements
         public abstract void Update();
 
         public abstract void Draw();
+
+        public new void Dispose()
+        {
+            InputManager.RemoveEventsFromObject(this);
+
+            base.Dispose();
+        }
 
         public virtual void Resize(DeviceContext deviceContext)
         {
--- save_switcher/Elements/SimpleButton.cs
+++ save_switcher/Elements/SimpleButton.cs
@@ -7,16 +7,28 @@ using System.Windows.Forms;
 
 namespace save_switcher.Elements
 {
+    public struct SimpleButtonProperties
+    {
+        public Vector2 Position;
+        public Size2 Size;
+
+        public string Text;
+        public float FontSize;
+        public Color TextColor;
+
+        public float BorderThickness;
+        public float CornerRadius;
+
+        public Color ButtonColor;
+        public Color ButtonHoverColor;
+        public Color ButtonPressedColor;
+        public Color BorderColor;
+        public Color BorderHoverColor;
+        public Color BorderPressedColor;
+    }
 
     internal class SimpleButton : Button, IDisposable
     {
-        private enum InputType
-        {
-            Mouse,
-            Keyboard,
-            Controller,
-        }
-
         private SimpleButtonProperties properties;
 
         private SolidColorBrush colorBrush;
@@ -25,17 +37,11 @@ namespace save_switcher.Elements
 
         private (Color button, Color border) currentColors;
 
-        private Action buttonAction;
-
-        private Vector2 lastMousePosition;
-        private InputType lastInputType;
-        private bool cursorWithinBounds = false;
 
         public SimpleButton(DeviceContext deviceContext, SimpleButtonProperties properties, Action buttonAction) 
             : base (deviceContext, properties.Position, properties.Size)
         {
             this.properties = properties;
-            this.buttonAction = buttonAction;
 
             currentColors = (properties.ButtonColor, properties.BorderColor);
 
@@ -43,54 +49,11 @@ namespace save_switcher.Elements
 
             createSizeDependantResources();
 
-            Program.GetProgramForm().MouseDown += OnMouseDown;
-            Program.GetProgramForm().MouseUp += OnMouseUp;
-            Program.GetProgramForm().KeyDown += OnKeyDown;
-            Program.GetProgramForm().KeyUp += OnKeyUp;
-        }
-
-        public void OnMouseDown(object sender, MouseEventArgs e)
-        {
-            if (CurrentSelectedObject == this && cursorWithinBounds)
-                pressButton();
-
-        }
-
-        public void OnMouseUp(object sender, MouseEventArgs e)
-        {
-            if(CurrentSelectedObject == this)
-                currentColors = (properties.ButtonHoverColor, properties.BorderHoverColor);
-
-        }
-
-        public void OnKeyDown(object sender, KeyEventArgs e)
-        {
-            void handleEvent(Action a)
-            {
-                a.Invoke();
-                e.Handled = true;
-            }
-
-            if (CurrentSelectedObject == this && e.Handled == false)
-            {
-                if (e.KeyCode == Keys.Left)
-                    handleEvent(() => SelectNeighbor(NavigateDirection.Left));
-                else if (e.KeyCode == Keys.Right)
-                    handleEvent(() => SelectNeighbor(NavigateDirection.Right));
-                else if (e.KeyCode == Keys.Up)
-                    handleEvent(() => SelectNeighbor(NavigateDirection.Up));
-                else if (e.KeyCode == Keys.Down)
-                    handleEvent(() => SelectNeighbor(NavigateDirection.Down));
-                else if (e.KeyCode == Keys.Enter)
-                    handleEvent(() => pressButton());
-            }
-
-        }
-
-        public void OnKeyUp(object sender, KeyEventArgs e)
-        {
-            if (CurrentSelectedObject == this)
-                currentColors = (properties.ButtonHoverColor, properties.BorderHoverColor);
+            OnHover += () => { currentColors = (properties.ButtonHoverColor, properties.BorderHoverColor); };
+            OnPressed += () => { 
+                currentColors = (properties.ButtonPressedColor, properties.BorderPressedColor);
+                buttonAction.Invoke();
+            };
         }
 
         public override void Deselect()
@@ -98,15 +61,12 @@ namespace save_switcher.Elements
             currentColors = (properties.ButtonColor, properties.BorderColor);
         }
 
-        public void Dispose()
+        public new void Dispose()
         {
-            Program.GetProgramForm().MouseDown -= OnMouseDown;
-            Program.GetProgramForm().MouseUp -= OnMouseUp;
-            Program.GetProgramForm().KeyDown -= OnKeyDown;
-            Program.GetProgramForm().KeyUp -= OnKeyUp;
-
             buttonText.Dispose();
             colorBrush.Dispose();
+
+            base.Dispose();
         }
 
         public override void Draw()
@@ -135,36 +95,7 @@ namespace save_switcher.Elements
             currentColors = (properties.ButtonHoverColor, properties.BorderHoverColor);
         }
 
-        public override void Update()
-        {
-            Form activeForm = Form.ActiveForm;
-            if (activeForm == null)
-                return;
-
-            System.Drawing.Point currentMousePos = Cursor.Position;
-            System.Drawing.Point mouseToScreen = activeForm.PointToClient(currentMousePos);
-            Vector2 mousePos = new Vector2(mouseToScreen.X, mouseToScreen.Y);
-
-            if (lastInputType == InputType.Mouse && mousePos.X >= properties.Position.X &&
-                mousePos.X <= properties.Position.X + properties.Size.Width && mousePos.Y >= properties.Position.Y &&
-                mousePos.Y <= properties.Position.Y + properties.Size.Height)
-            {
-                if (CurrentSelectedObject != this)
-                {
-                    Select();
-                }
-
-                cursorWithinBounds = true;
-            }
-            else
-                cursorWithinBounds = false;
-
-            if (mousePos != lastMousePosition)
-                lastInputType = InputType.Mouse;
-
-            lastMousePosition = mousePos;
-
-        }
+        public override void Update() { }
 
         private void createSizeDependantResources()
         {
@@ -177,32 +108,5 @@ namespace save_switcher.Elements
             buttonText.ParagraphAlignment = ParagraphAlignment.Center;
             buttonText.TextAlignment = TextAlignment.Center;
         }
-
-        private void pressButton()
-        {
-            currentColors = (properties.ButtonPressedColor, properties.BorderPressedColor);
-
-            buttonAction.Invoke();
-        }
-    }
-
-    public struct SimpleButtonProperties
-    {
-        public Vector2 Position;
-        public Size2 Size;
-
-        public string Text;
-        public float FontSize;
-        public Color TextColor;
-
-        public float BorderThickness;
-        public float CornerRadius;
-
-        public Color ButtonColor;
-        public Color ButtonHoverColor;
-        public Color ButtonPressedColor;
-        public Color BorderColor;
-        public Color BorderHoverColor;
-        public Color BorderPressedColor;
     }
 }
